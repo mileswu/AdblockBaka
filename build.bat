1bea15cfba33b56486c5fef98d5d64d0e2d03350
root <miles.wu@wu-home.co.uk>
2016-04-20 07:42:01 +0000

    Add build script

---
+++ build.bat
@@ -0,0 +1,3 @@
+xcopy src AdblockBaka.extension /e /i /h
+icacls "AdblockBaka.extension" /grant "*S-1-15-2-3624051433-2125758914-1423191267-1740899205-1073925389-3782572162-737981194":"(OI)(CI)(WDAC,WO,GE)"
+
